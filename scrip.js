4751ce58bb24fd894ac2a16667adf5a8055880a1
Ayo Daramola <103411493+dultimategithub@users.noreply.github.com>
2022-04-11 10:36:38 +0100

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,33 @@
+var operator = prompt("please, enter the operation to perform: ");
+
+var firstNumber = Number(prompt("Enter the first number: "));
+var firstNumber = Number(prompt("Enter the second number: "));
+
+if (operator==="add" || operator==="Add")
+{
+    var sum = firstNumber + secondNumber;
+    alert ("Sum of two numbers is: " + sum);
+}
+
+else if (operator==="subtract" || operator==="subtract")
+{
+    var sub = firstNumber + secondNumber;
+    alert ("Sub of two numbers is: " - subtract);
+}
+
+else if (operator==="multiplication" || operator==="multiplication")
+{
+    var product = firstNumber * secondNumber;
+    alert ("Product of two numbers is: " * multiplication);
+}
+
+else if (operator==="multiplication" || operator==="multiplication")
+{
+    var div = firstNumber / secondNumber;
+    alert ("Div of two numbers is: " / division);
+}
+
+else
+{
+    alert ("invalid input");
+}
